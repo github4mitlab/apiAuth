531d354ac7ec76bd90353040fdaa027ddf554f4e
surfaceMay <may1516@gmail.com>
2019-06-08 11:05:30 +0900

    Add JWT

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,16 @@
+const JWT = require("jsonwebtoken");
 const userModel = require("../models/user");
+
+const { JWT_SECRET } = require("../config");
+
+signToken = user => {
+    return JWT.sign({
+        iss: "mayLee",
+        sub: user.id,
+        iat: new Date().getTime(),
+        exp: new Date().setDate(new Date().getDate() +1) // 현재 시간 더하기 하루
+    }, JWT_SECRET);
+};
 
 module.exports = {
     singUp: async (req, res, next) => {
@@ -11,9 +23,12 @@ module.exports = {
         const newUser = new userModel({ email, password});
         await newUser.save();
 
-        // Respond with token
+        // Generate the token
+        const token = signToken(newUser);
+
+        // Respond with token : 파싱확인은 https://jwt.io/
         res.status(200).json({
-            user: 'Created'
+            user: token
         });
     },
     singIn: async (req, res, next) => {
