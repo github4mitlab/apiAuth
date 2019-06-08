0d022273f8de6d394c23ee23bd36edeabdd92507
surfaceMay <may1516@gmail.com>
2019-06-08 10:55:16 +0900

    Add Signup Function

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,20 @@
+const userModel = require("../models/user");
+
 module.exports = {
     singUp: async (req, res, next) => {
-        // console.log(req.value.body);
-        console.log("signup");
+        const { email, password } = req.body; 
+        const foundUser = await userModel.findOne({email});
+        if(foundUser) {
+            return res.status(403).json({ error: "Email is already in use"});
+        }
+        //Create a new user
+        const newUser = new userModel({ email, password});
+        await newUser.save();
+
+        // Respond with token
+        res.status(200).json({
+            user: 'Created'
+        });
     },
     singIn: async (req, res, next) => {
         // console.log("SignIn");
